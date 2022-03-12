b6576f457da3b52e5f36ac76aa1467b5ee893df7
sergiusgeoffrey <73834938+sergiusgeoffrey@users.noreply.github.com>
2022-03-12 20:52:24 +0700

    V3.5 Update Gaji Dosen

--- bin/dosen.dart
+++ bin/dosen.dart
@@ -3,21 +3,33 @@ import 'pegawai.dart';
 class DosenLB extends Pegawai{
   DosenLB(String? nama, int usia, int sks,int gaji,int tunjangan) : super(nama, usia, sks,gaji,tunjangan){
     //Contoh Gaji Dasar
-    this.gaji=0+(sks*40000);
+    this.gaji=gaji;
     this.tunjangan=tunjangan;
   }
+  int getGaji(){
+    return sks * 40000;
+  }
+  
 }
 class DosenTamu extends Pegawai{
   DosenTamu(String? nama, int usia, int sks,int gaji,int tunjangan) : super(nama, usia, sks,gaji,tunjangan){
     //Contoh Gaji Dasar
-    this.gaji=0+(sks*40000);
+    this.gaji=0;
     this.tunjangan=tunjangan;
+  }
+
+  int getGaji(){
+    return tunjangan + (sks * 40000);
   }
 }
 class DosenTetap extends Pegawai{
   DosenTetap(String? nama, int usia, int sks,int gaji,int tunjangan) : super(nama, usia, sks,gaji,tunjangan){
     //Contoh Gaji Dasar
-    this.gaji=gaji+(sks*40000);
+    this.gaji=gaji;
     this.tunjangan=tunjangan;
+  }
+
+  int getGaji(){
+    return gaji + tunjangan + (sks * 40000);
   }
 }
--- bin/main.dart
+++ bin/main.dart
@@ -5,6 +5,7 @@ import 'dosen.dart';
 void main(List<String> arguments) {
   var mh1=new Mahasiswa('Gejo', 21, 109, 3, 4);
   print(mh1.nama);
-  //var dosenLB1 = new DosenLB(nama, usia, sks, gaji, tunjangan, kehadiran)
+  var dosenLB1=new DosenLB('Yo', 22, 100, 0, 100000);
+  print(dosenLB1.getGaji());
   //var staff1= new Staff('Gejo', usia, sks, gaji, tunjangan, kehadiran)
 }
